848b6a25c76450ebf1847472020eac30ef1208e9
Ronald Barbosa <ronaldverissimo@hotmail.com>
2026-04-13 15:59:16 -0300

    feat: bootrap project with initial structure; add tailwind

---
+++ src/app/components/layout/layout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from "./main/main.component";
+
+@Component({
+  selector: 'app-layout',
+  imports: [HeaderComponent, FooterComponent, MainComponent],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {
+
+}
