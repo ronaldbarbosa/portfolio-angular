c38761ea2f8eae936991b5c14cff6a8346049758
Ronald Barbosa <ronaldverissimo@hotmail.com>
2026-04-14 14:30:47 -0300

    feat: finished implementation of home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,18 @@
-import { afterNextRender, AfterViewInit, Component, TemplateRef, ViewChild } from '@angular/core';
+import { afterNextRender, Component, TemplateRef, ViewChild } from '@angular/core';
 import { LayoutComponent } from '../../components/layout/layout.component';
 import { ContentService } from '../../services/content.service';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [LayoutComponent],
+  imports: [NgOptimizedImage, LayoutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   @ViewChild('homeContent', { static: true }) homeContent!: TemplateRef<any>;
+  imagemUrl = '/images/perfil.png';
+  nomeArquivo = 'foto-perfil';
 
   constructor(private contentService: ContentService) {
     // afterNextRender garante que o DOM está pronto
